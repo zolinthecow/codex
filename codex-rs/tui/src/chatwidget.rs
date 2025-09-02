eb40fe3451d6ce63c961877f4fe190d1235ed8f3
Ahmed Ibrahim <aibrahim@openai.com>
2025-09-02 10:59:07 -0700

    Add logs to know when we users are changing the model (#3060)

--- codex-rs/tui/src/chatwidget.rs
+++ codex-rs/tui/src/chatwidget.rs
@@ -1082,6 +1082,7 @@ impl ChatWidget {
             let is_current = preset.model == current_model && preset.effort == current_effort;
             let model_slug = preset.model.to_string();
             let effort = preset.effort;
+            let current_model = current_model.clone();
             let actions: Vec<SelectionAction> = vec![Box::new(move |tx| {
                 tx.send(AppEvent::CodexOp(Op::OverrideTurnContext {
                     cwd: None,
@@ -1093,6 +1094,13 @@ impl ChatWidget {
                 }));
                 tx.send(AppEvent::UpdateModel(model_slug.clone()));
                 tx.send(AppEvent::UpdateReasoningEffort(effort));
+                tracing::info!(
+                    "New model: {}, New effort: {}, Current model: {}, Current effort: {}",
+                    model_slug.clone(),
+                    effort,
+                    current_model,
+                    current_effort
+                );
             })];
             items.push(SelectionItem {
                 name,
