639a6fd2f39d45b995213118c21b63853e519f13
Michael Bolin <mbolin@openai.com>
2025-09-24 08:32:00 -0700

    chore: upgrade to Rust 1.90 (#4124)
    Inspired by Dependabot's attempt to do this:
    https://github.com/openai/codex/pull/4029
    
    The new version of Clippy found some unused structs that are removed in
    this PR.
    
    Though nothing stood out to me in the Release Notes in terms of things
    we should start to take advantage of:
    https://blog.rust-lang.org/2025/09/18/Rust-1.90.0/.

--- codex-rs/core/src/client.rs
+++ codex-rs/core/src/client.rs
@@ -416,9 +416,6 @@ struct SseEvent {
 }
 
 #[derive(Debug, Deserialize)]
-struct ResponseCreated {}
-
-#[derive(Debug, Deserialize)]
 struct ResponseCompleted {
     id: String,
     usage: Option<ResponseCompletedUsage>,
